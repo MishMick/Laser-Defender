cff05c28c6986decb762e1d7b20f7c84456c26ff
MishMick <mishallovic@gmail.com>
2015-09-26 17:28:45 +0530

    26/09/15

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+    public GameObject enemy;
+	// Use this for initialization
+	void Start () {
+	  Instantiate(enemy,transform.position,Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+    private float speed = 10.0f;
+    private float CameraDistance;
+    static private float xmin,xmax;
+    static private float padding = 0.5f;
+    static private float repeatRate = 0.2f;
+    public float projectileSpeed;
+    //Shooting spree
+    public GameObject laser;
+	// Use this for initialization
+	void Start () 
+	{
+	  CameraDistance = transform.position.z-Camera.main.transform.position.z;
+	  Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,CameraDistance));
+	  Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,CameraDistance));
+	  xmin = leftmost.x;
+	  xmax = rightmost.x;
+	}
+	void movement()
+	{
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.position += Vector3.left*speed*Time.deltaTime;		
+		}
+		if(Input.GetKey(KeyCode.RightArrow))
+		{	    
+			transform.position += Vector3.right*speed*Time.deltaTime;
+		}
+		float newX = Mathf.Clamp(transform.position.x,xmin+padding,xmax-padding);
+		transform.position = new Vector3(newX,transform.position.y,transform.position.z);
+	}
+	void Fire()
+	{
+		GameObject beam = Instantiate(laser,transform.position,Quaternion.identity) as GameObject;
+		beam.GetComponent<Rigidbody2D>().velocity = new Vector3(0,projectileSpeed);
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+		   InvokeRepeating("Fire",0.0001f,repeatRate);
+		}
+		if(Input.GetKeyUp(KeyCode.Space))
+		{
+		   CancelInvoke();
+		}
+	  movement();
+	}
+}
